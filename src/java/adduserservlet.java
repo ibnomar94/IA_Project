30bbe9577355d14148c407d0b24c66e6d379c9e4
ibnomar <ibnomar@ibnomar>
2016-12-21 22:19:31 +0200

    Adding new user function

--- src/java/adduserservlet.java
+++ src/java/adduserservlet.java
@@ -11,6 +11,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.io.*;
+import static java.util.jar.Pack200.Packer.PASS;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javax.mail.Flags.Flag.USER;
+
 
 /**
  *
@@ -29,19 +36,42 @@ public class adduserservlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet adduserservlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet adduserservlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String username = "root";
+            String pass = "";
+            String JDBC_DRIVER="com.mysql.jdbc.Driver";  
+            String DB_URL="jdbc:mysql://localhost/Ia_library";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(DB_URL, username, pass);
+            Statement stmt = conn.createStatement();
+            String sql;
+            
+            String id = request.getParameter("id");
+            String name = request.getParameter("name");
+            String user_name = request.getParameter("username");
+            String password = request.getParameter("password");
+            String email = request.getParameter("email");
+            
+            sql = "INSERT INTO `student`(`id`, `name`, `username`, `email`, `password`) VALUES ('"+id+"','"+name+"','"+user_name+""
+                    + "','"+email+"','"+password+"')";
+            int check  ;
+            try {
+               check = stmt.executeUpdate(sql);
+               if(check!=0){
+               String msg="Record has been inserted";
+               out.println("<font size='6' color=blue>" + msg + "</font>");
+               
+            }
+            }
+            catch(Exception e){             
+                out.println(e.getMessage());
+            }
+
+            stmt.close();
+            conn.close();
         }
     }
 
@@ -57,7 +87,13 @@ public class adduserservlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(adduserservlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(adduserservlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -71,7 +107,13 @@ public class adduserservlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(adduserservlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(adduserservlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- web/newuser.jsp
+++ web/newuser.jsp
@@ -16,12 +16,12 @@
     <body>
         <div>
         <form action="adduserservlet" align="center">
-                    <label> ID: </label>  <input type="text" name="id" required >
+                    <label> ID: </label>  <input type="text" name="id" pattern="201.+[0-9]{8}" title="8 digits Starting with 201" required >
                     <label> Name: </label>  <input type="text" name="name" required >
                     <label> User Name: </label>  <input type="text" name="username" required > 
                     <label> Password: </label>  <input type="password" name="password" required >
                     <label> Email </label>  <input type="email" name="email" required >
-                    <input type="submit" value="Login"> 
+                    <input type="submit" value="Add User"> 
                 </form>
         </div>
     </body>
